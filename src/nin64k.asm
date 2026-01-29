8d46f9ced7fc1cbede18f83f3e6e3cf8daefb354
dmd <diamond-msc@users.noreply.github.com>
2026-01-29 10:00:40 +0100

    Implement n64k for new player/format

--- src/nin64k.asm
+++ src/nin64k.asm
@@ -5,8 +5,9 @@
 .setcpu "6502"
 
 ; Zero page
-zp_load_flag     = $79
 zp_part_num     = $7B
+zp_preloaded    = $7C              ; Song number that's been preloaded
+zp_last_line    = $0D
 
 ; Decompressor zero page (external interface)
 zp_src_lo       = $02
@@ -15,86 +16,128 @@ zp_bitbuf       = $04
 zp_out_lo       = $05
 zp_out_hi       = $06
 
-; State
-zp_last_line    = $0d
-
-; Tune entry points
-TUNE1_INIT      = $1000
-TUNE1_PLAY      = $1003
-TUNE2_INIT      = $7000
-TUNE2_PLAY      = $7003
+; Buffer addresses (new format)
+TUNE1_BASE      = $1800         ; Odd songs (1,3,5,7,9)
+TUNE2_BASE      = $6800         ; Even songs (2,4,6,8)
 
 .segment "LOADADDR"
         .word   $0801
 
 .segment "CODE"
 
-
 ; ----------------------------------------------------------------------
-; BASIC stub: SYS 2066
+; BASIC stub: SYS 2061
 ; ----------------------------------------------------------------------
 basic_stub:
         .word   $0810               ; Pointer to next BASIC line
         .word   8580                ; Line number
         .byte   $9E                 ; SYS token
-        .byte   "2061"              ; SYS address + decoration
+        .byte   "2061"              ; SYS address
         .byte   $00                 ; End of line
         .word   $0000               ; End of BASIC program
 
 ; ----------------------------------------------------------------------------
 start:
-        jsr     init_game
+        sei
+        ; Set up safe IRQ vector for all-RAM mode
+        lda     #<safe_rti
+        sta     $FFFE
+        lda     #>safe_rti
+        sta     $FFFF
+        ; Disable CIA interrupts
+        lda     #$7F
+        sta     $DC0D
+        lda     $DC0D
+
+        ; Switch to all-RAM for stream copy
+        lda     #$30
+        sta     $01
+        jsr     copy_streams
+        jsr     init_stream
+
+        ; Init player for song 1
         lda     #1
         sta     zp_part_num
-        jsr     setup_irq
-        jsr     init_stream
-        lda     #0
-        jsr     TUNE1_INIT
+        sta     zp_preloaded
+        lda     #$00
+        ldx     #>TUNE1_BASE
+        jsr     player_init
 
+        cli
+
+; ----------------------------------------------------------------------------
+; Main loop - polling-based playback with preloading
 ; ----------------------------------------------------------------------------
 main_loop:
         jsr     checkpoint
         lda     #$35
         sta     $01
-        lda     zp_load_flag
-        bne     do_load_next
-        ; Direct keyboard read (CIA1 at $DC00/$DC01, I/O already banked in)
-        lda     #$7F                ; Select row 7
+        ; Check space bar for skip
+        lda     #$7F                ; Select keyboard row 7
         sta     $DC00
         lda     $DC01
         and     #$10                ; Check bit 4 (space bar)
-        bne     main_loop           ; Not pressed
+        bne     @check_preload      ; Not pressed
+        ; Space pressed - debounce
 @debounce:
         jsr     checkpoint
         lda     #$35
         sta     $01
-        lda     $DC01               ; Wait for release
+        lda     $DC01
         and     #$10
-        beq     @debounce
+        beq     @debounce           ; Wait for release
         ; Reset SID
         ldx     #$18
         lda     #$00
 @sid:   sta     $D400,x
         dex
         bpl     @sid
-        ; Advance to next song
+        ; Skip to next song if preloaded
         lda     zp_part_num
-        cmp     #$09                ; Stop at song 9
-        beq     main_loop
-        lda     #$FF
-        sta     zp_load_flag
+        cmp     #$09
+        beq     @check_preload      ; Already at song 9
+        cmp     zp_preloaded
+        bcs     @check_preload      ; Next not preloaded yet
+        ; Switch to preloaded song
         inc     zp_part_num
+        lda     zp_part_num
+        and     #$01
+        bne     @init_odd
+        ; Even part num (2,4,6,8) -> buffer at $6800
+        lda     #$00
+        ldx     #>TUNE2_BASE
+        jsr     player_init
+        jmp     @check_preload
+@init_odd:
+        ; Odd part num (3,5,7,9) -> buffer at $1800
+        lda     #$00
+        ldx     #>TUNE1_BASE
+        jsr     player_init
+@check_preload:
+        lda     #$30
+        sta     $01
+        ; Check if preload needed
+        lda     zp_part_num
+        cmp     zp_preloaded
+        bcc     main_loop           ; Already preloaded
+        jsr     do_preload
         jmp     main_loop
 
+safe_rti:
+        rti
+
+; ----------------------------------------------------------------------------
+; Checkpoint - called during decompression for vblank detection and indicator
+; ----------------------------------------------------------------------------
 checkpoint:
         lda     #$35
         sta     $01
-        rol     $D020
+        rol     $D020               ; Loading indicator
         lda     $D012
         bmi     @no_vblank
         cmp     zp_last_line
         bpl     @no_vblank
-        jsr     play_tick_if_ready
+        jsr     play_frame
         lda     #0
 @no_vblank:
         sta     zp_last_line
@@ -103,37 +146,17 @@ checkpoint:
         rts
 
 ; ----------------------------------------------------------------------------
-do_load_next:
-        lda     #$CC
-        sta     $0427
-        jsr     load_and_init
-        lda     #$00
-        sta     zp_load_flag
-        lda     #$20
-        sta     $0427
-        jmp     main_loop
-
+; Play one frame of music
 ; ----------------------------------------------------------------------------
-setup_irq:
-        sei
-        lda     #$35                ; I/O on, KERNAL off
-        sta     $01
-        lda     #$7F
-        sta     $DC0D               ; Disable CIA interrupts
-        lda     $DC0D               ; Acknowledge pending CIA
-        cli
-        rts
-
-; ----------------------------------------------------------------------------
-
-play_tick_if_ready:
+play_frame:
         lda     #$07
         sta     $D020
         txa
         pha
         tya
         pha
-        jsr     play_tick
+        jsr     player_play
+        jsr     check_countdown
         pla
         tay
         pla
@@ -142,16 +165,9 @@ play_tick_if_ready:
         sta     $D020
         rts
 
-play_tick:
-        lda     zp_part_num
-        and     #$01
-        beq     @play_even
-        jsr     $1003
-        jmp     check_countdown
-
-@play_even:
-        jsr     $7003
-
+; ----------------------------------------------------------------------------
+; Check countdown timer, switch to preloaded song when expired
+; ----------------------------------------------------------------------------
 check_countdown:
         lda     zp_part_num
         asl     a
@@ -168,129 +184,101 @@ check_countdown:
         dec     part_times+1,x
 @check_zero:
         lda     part_times,x
-        bne     play_done
+        bne     @done
         lda     part_times+1,x
-        bne     play_done
-        lda     zp_part_num
-        cmp     #$09                ; Stop at song 9
-        beq     play_done
-        lda     #$FF
-        sta     zp_load_flag
-        inc     zp_part_num
-
-; ----------------------------------------------------------------------------
-play_done:
-        rts
-
-; ----------------------------------------------------------------------
-; Copy streams wrapper with banking
-; ----------------------------------------------------------------------
-copy_streams_banked:
-        sei                         ; Must disable IRQs when KERNAL banked out
-        lda     #$30                ; All RAM (including $D000-$DFFF, no I/O)
-        sta     $01
-        jsr     copy_streams
-        lda     #$37                ; Restore ROMs
-        sta     $01
-        cli                         ; Re-enable IRQs
-        rts
-
-; ----------------------------------------------------------------------------
-load_and_init:
+        bne     @done
         lda     zp_part_num
         cmp     #$09
-        bcs     @done
-        jsr     decompress_next
-        lda     #$A1
-        sta     $0427
+        beq     @done
+        ; Song ended - switch to preloaded song immediately
+        inc     zp_part_num
+        ; Init player for the preloaded buffer
         lda     zp_part_num
         and     #$01
-        bne     @init_buf2
+        bne     @switch_odd
+        ; Even part num (2,4,6,8) -> buffer at $6800
         lda     #$00
-        jmp     TUNE1_INIT
-@init_buf2:
+        ldx     #>TUNE2_BASE
+        jsr     player_init
+        jmp     @done
+@switch_odd:
+        ; Odd part num (3,5,7,9) -> buffer at $1800
         lda     #$00
-        jmp     TUNE2_INIT
+        ldx     #>TUNE1_BASE
+        jsr     player_init
 @done:
         rts
 
-; ----------------------------------------------------------------------
-; Part timing data (decremented in place during playback)
-; ----------------------------------------------------------------------
+; ----------------------------------------------------------------------------
+; Preload next song (decompress to alternate buffer)
+; ----------------------------------------------------------------------------
+do_preload:
+        ldx     zp_part_num
+        cpx     #9
+        bcs     @done               ; No preload after song 9
+        lda     #$CC                ; Loading indicator on screen
+        sta     $0427
+        inx                         ; Next song number
+        txa
+        pha                         ; Save next song number
+        ; Decompress to alternate buffer
+        lda     #$00
+        sta     zp_out_lo
+        txa
+        and     #$01
+        bne     @preload_odd
+        ; Even song -> $6800
+        lda     #>TUNE2_BASE
+        bne     @decompress
+@preload_odd:
+        ; Odd song -> $1800
+        lda     #>TUNE1_BASE
+@decompress:
+        sta     zp_out_hi
+        jsr     decompress
+        pla                         ; Recover next song number
+        sta     zp_preloaded        ; Mark as preloaded
+        lda     #$20                ; Clear loading indicator
+        sta     $0427
+@done:
+        rts
+
+; ----------------------------------------------------------------------------
+; Part timing data
+; ----------------------------------------------------------------------------
 part_times:
 .include "part_times.inc"
 
 ; ----------------------------------------------------------------------------
-; Initialize stream pointer to song 2 (song 1 is pre-decompressed at $1000)
+; Initialize stream pointer to song 2
+; Stream is included with STREAM_OFFSET=3026 so byte 0 = original byte 3026
+; Song 1 = 24210 bits = byte 3026, bit 2 of original stream
 ; ----------------------------------------------------------------------------
 init_stream:
-        lda     #<(STREAM_MAIN_DEST + 1)
-        sta     zp_src_lo
-        lda     #>(STREAM_MAIN_DEST + 1)
-        sta     zp_src_hi
-        lda     STREAM_MAIN_DEST
+        lda     STREAM_DEST             ; First byte of offset stream
+        sec
+        rol     a
         asl     a
-        asl     a
-        asl     a
-        asl     a
-        asl     a
-        ora     #$10
         sta     zp_bitbuf
-        rts
-
-; ----------------------------------------------------------------------------
-; Decompress next song from stream
-; ----------------------------------------------------------------------------
-decompress_next:
-        ; Set output destination based on part number
-        lda     #$00
-        sta     zp_out_lo
-        lda     zp_part_num
-        and     #$01
-        beq     @even_part
-        lda     #$70                ; Odd part num -> $7000
-        bne     @do_decompress
-@even_part:
-        lda     #$10                ; Even part num -> $1000
-@do_decompress:
-        sta     zp_out_hi
-        ; Call decompressor in all-RAM mode (stream spans I/O region)
-        lda     #$30                ; All RAM
-        sta     $01
-        jsr     decompress
-        ; Song 9 is split: stream_main has first part, stream_tail has rest
-        lda     zp_part_num
-        cmp     #$08                ; Song 9 = part 8
-        bne     @load_done
-        lda     #<STREAM_TAIL_DEST
+        lda     #<(STREAM_DEST + 1)
         sta     zp_src_lo
-        lda     #>STREAM_TAIL_DEST
+        lda     #>(STREAM_DEST + 1)
         sta     zp_src_hi
-        lda     #$80
-        sta     zp_bitbuf
-        jsr     decompress
-@load_done:
-        lda     #$35                ; Back to I/O mode
-        sta     $01
-        clc                         ; Success
         rts
 
 ; ============================================================================
-; V23 Decompressor - generated by ./compress
-; Setup: zp_src, zp_bitbuf=$80, zp_out ($1000 or $7000)
+; Decompressor (calls checkpoint for vblank detection during decompression)
 ; ============================================================================
 .include "../generated/decompress.asm"
 
-; ----------------------------------------------------------------------------
-init_game:
-        jsr     copy_streams_banked
-        lda     #$FF
-        sta     zp_load_flag
-        rts
+; ============================================================================
+; Standalone player (new format)
+; ============================================================================
+.include "odin_player.inc"
 
 .segment "PART1"
 .incbin "../generated/part1.bin"
 
 .segment "DATA"
-STREAM_OFFSET = 4997
+STREAM_OFFSET = 3026            ; Skip song 1's compressed data (pre-loaded from part1.bin)
 .include "stream.inc"
